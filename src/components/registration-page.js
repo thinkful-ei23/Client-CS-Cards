7e480a9a00f26e1e9d35b13f4218ba20d598f602
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2018-10-15 14:16:02 -0400

    Changed Foo the CS Cards in Dashboard.js, Header-bar.js, and Registration-form.js

--- src/components/registration-page.js
+++ src/components/registration-page.js
@@ -12,7 +12,7 @@ export function RegistrationPage(props) {
     }
     return (
         <div className="home">
-            <h2>Register for Foo App</h2>
+            <h2>Register for CS Cards</h2>
             <RegistrationForm />
             <Link to="/">Login</Link>
         </div>
