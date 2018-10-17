099966bc203024e6989cdbe36f463ee477d26b1b
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2018-10-17 16:55:01 -0400

    changed registration-form.css

--- src/components/registration-form.js
+++ src/components/registration-form.js
@@ -22,11 +22,11 @@ export class RegistrationForm extends React.Component {
     render() {
         return (
             <form
-                className="login-form"
+                className="registration-form"
                 onSubmit={this.props.handleSubmit(values =>
                     this.onSubmit(values)
                 )}>
-                <label htmlFor="firstName">First name</label>
+                <label htmlFor="firstName">First Name</label>
                 <Field component={Input} type="text" name="firstName" />
                 <label htmlFor="username">Username</label>
                 <Field
@@ -42,7 +42,7 @@ export class RegistrationForm extends React.Component {
                     name="password"
                     validate={[required, passwordLength, isTrimmed]}
                 />
-                <label htmlFor="passwordConfirm">Confirm password</label>
+                <label htmlFor="passwordConfirm">Confirm Password</label>
                 <Field
                     component={Input}
                     type="password"
@@ -50,6 +50,7 @@ export class RegistrationForm extends React.Component {
                     validate={[required, nonEmpty, matchesPassword]}
                 />
                 <button
+                    className='registration-button'
                     type="submit"
                     disabled={this.props.pristine || this.props.submitting}>
                     Register
--- src/components/registration-page.js
+++ src/components/registration-page.js
@@ -12,7 +12,7 @@ export function RegistrationPage(props) {
     }
     return (
         <div className="home">
-            <h2>Register for CS Cards</h2>
+            <h2>Registration</h2>
             <RegistrationForm />
             <Link to="/">Login</Link>
         </div>
