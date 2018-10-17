290b4dd5869778351b4090a37ad32a46a0d4e1a6
john <johnjacobcosgrove@gmail.com>
2018-10-17 16:43:48 -0400

    messing with css

--- src/components/ReadMe.js
+++ src/components/ReadMe.js
@@ -3,10 +3,10 @@ import React from 'react'
 export default function ReadMe(props){
     return(
         <div className='readme'>
-            <p>CS cards is a interactive learning app using space repetition</p>
-            <p>that will allow you to quickly and effectively learn many of</p>
-            <p>those hard to grasp Computer Science concepts! Simply create</p>
-            <p>a account and start quizzing yourself.</p>
+            <p>CS cards is a interactive learning app using space repetition
+            that will allow you to quickly and effectively learn many of
+            those hard to grasp Computer Science concepts!<br/> Simply create
+            a account and start quizzing yourself.</p>
         </div>
     )
 }
--- src/components/login-form.js
+++ src/components/login-form.js
@@ -41,7 +41,7 @@ export class LoginForm extends React.Component {
                     id="password"
                     validate={[required, nonEmpty]}
                 />
-                <button disabled={this.props.pristine || this.props.submitting}>
+                <button className="login-button" disabled={this.props.pristine || this.props.submitting}>
                     Log in
                 </button>
             </form>
