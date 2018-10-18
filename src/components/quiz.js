a72d3572cf814c2405e82477162a0bccc40d850b
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2018-10-18 15:50:45 -0400

    css changes

--- src/components/quiz.js
+++ src/components/quiz.js
@@ -28,23 +28,32 @@ export class Quiz extends React.Component {
     }
     let answers
     if(this.props.answer != null && this.props.answer.answer === 'correct'){
-      answers = 'correct-answer'
+      answers = 'correct-answer';
     }else{
-      answers = 'incorrect-answer'
+      answers = 'incorrect-answer';
     }
-
+    let questionFontSize;
+    if (this.props.question) {
+      if (this.props.question.length < 220 &&       this.props.question.length > 170) {
+        questionFontSize = 'question-text-high';
+      } else if (this.props.question.length < 170) {
+        questionFontSize = 'question-text-low';
+      } else {
+        questionFontSize = '';
+      }
+    }
     if (this.props.answer) {
       return (
       <div className='quiz'>
-        <p className={answers}>{this.props.answer.answer}</p>
-        <p><span className="correct-answer">{this.props.answer.correctAnswer}</span> : {this.props.question}</p>
+        <p className={`answer ${answers}`}>{this.props.answer.answer}</p>
+        <p className='answer-text'><span className="correct-answer">{this.props.answer.correctAnswer}</span> : {this.props.question}</p>
         <button className='next-button' onClick={() => this.fetchNextQuestion()}>Next</button>
       </div>
       );
     } else {
       return (
         <div className='quiz'>
-          <p className='quiz-question'>{question}</p>
+          <p className={`quiz-question ${questionFontSize}`}>{question}</p>
           <form className='quiz-form' onSubmit={(userAnswer) => submitAnswer(userAnswer)}>
             <input id='answer' className='quiz-input' ref={input => (userAnswer = input)} type='text'></input>
             <button className='quiz-button-submit' type='submit'>Submit</button>
